76b911ab40cf2f7b1a96a50d9761e68fd3ce6801
Saurabhk1975 <bickysaurabh545452@gmail.com>
2024-02-24 11:12:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const heading = React.createElement("div",
+{
+    id:"parent"
+},
+React.createElement("div",
+{
+    id:"child"
+},[
+React.createElement("h1",{id:"heading"},"inner text of h1 "),
+React.createElement("h5",{id:"heading"},"inner text of  h5")
+]
+
+));
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
